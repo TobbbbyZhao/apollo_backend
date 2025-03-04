61c70bd6a49d7a207bb0a5fd8611934d9eb96355
TobbbbyZhao <xuanzhizhao@gmail.com>
2025-03-04 15:51:16 -0500

    Initial commit - Node.js server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 490;
+
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Hello, World! Your Node.js server is running on 490.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
